002b46d2d5ce4d955c14197a2bca6a106bbf193d
tuan truong <tuan.truong@texo.design>
2022-10-24 16:06:52 +0700

    static public controller with pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_public#landing_page'
+  
+  get 'privacy', to: 'static_public#privacy'
+  get 'terms', to: 'static_public#terms'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
