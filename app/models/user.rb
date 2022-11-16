e40f87e699a24acde1a3dc97ec6f3e125b7290e7
tuan truong <tuan.truong@texo.design>
2022-11-16 13:30:15 +0700

    add omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,27 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
+    user = User.where(email: access_token.info.email).first
 
     # Uncomment the section below if you want users to be created if they don't exist
-    user ||= User.create(email: data['email'],
+    user ||= User.create(email: access_token.info.email,
                          password: Devise.friendly_token[0, 20])
+
+    user.provider = access_token.provider
+    user.uid = access_token.uid
+    user.name = access_token.info.name
+    user.image = access_token.info.image
+    user.save!
+
     user
   end
+
+  def username
+    if name?
+      name
+    else
+      email
+    end
+  end
+  
 end
