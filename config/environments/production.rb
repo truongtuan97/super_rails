b10809f6768a06042d92fd6a8e320c6d28278bc1
tuan truong <tuan.truong@texo.design>
2022-11-17 16:27:13 +0700

    add smtp into production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,19 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "super-rails-tt.herokuapp.com", protocol: 'https' }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "smtp.gmail.com",
+    port: 587,
+    user_name: Rails.application.credentials.dig(:smtp, :username),
+    password: Rails.application.credentials.dig(:smtp, :username),
+    authentication: "plain",
+    openssl_verify_mode: "none",
+    enable_starttls_auto: true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
